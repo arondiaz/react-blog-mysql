7870b42051b2c402fc9378a4e4a03832821ea4b0
Arón Díaz <arondiaz.cpn@gmail.com>
2024-10-24 00:52:16 -0300

    added lucide-react and implemented navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,41 @@
-
+import { Coffee } from "lucide-react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          {" "}
+          <Link to={"/"}>
+            <Coffee />
+          </Link>{" "}
+        </div>
+        <div className="links">
+          <Link to={"/?cat=code"}>
+            <h6>CODE</h6>
+          </Link>
+          <Link to={"/?cat=tech"}>
+            <h6>TECH</h6>
+          </Link>
+          <Link to={"/?cat=docs"}>
+            <h6>DOCS</h6>
+          </Link>
+          <Link to={"/?cat=work"}>
+            <h6>WORK</h6>
+          </Link>
 
-export default Navbar
+          <span>username</span>
+          <span>logout</span>
+          <span className="write">
+            <Link className="link" to={"/write"}>
+              Write
+            </Link>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
