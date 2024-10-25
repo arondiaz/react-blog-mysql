6ece9b35b7769891e082377a5bf7c75e25896387
Arón Díaz <arondiaz.cpn@gmail.com>
2024-10-24 23:43:27 -0300

    updated styles on home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,20 +4,20 @@ const Home = () => {
   const posts = [
     {
       id: 1,
-      title: "Lorem ipsum",
-      description: "Lorem",
+      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim vel ipsum commodi laboriosam.",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim vel ipsum commodi laboriosam voluptate quidem fugiat, cum voluptatibus consectetur ex officiis, dolorum numquam perspiciatis porro labore? Soluta sint odio minima.",
       img: "https://images.pexels.com/photos/5077042/pexels-photo-5077042.jpeg?auto=compress&cs=tinysrgb&w=600",
     },
     {
       id: 2,
-      title: "Lorem ipsum",
-      description: "Lorem",
+      title: "Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim vel ipsum commodi laboriosam Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim vel ipsum commodi laboriosam voluptate quidem fugiat, cum voluptatibus consectetur ex officiis, dolorum numquam perspiciatis porro labore? Soluta sint odio minima.",
       img: "https://images.pexels.com/photos/5077042/pexels-photo-5077042.jpeg?auto=compress&cs=tinysrgb&w=600",
     },
     {
       id: 3,
-      title: "Lorem ipsum",
-      description: "Lorem",
+      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim vel ipsum commodi laboriosam.",
+      description: "LoremLorem ipsum dolor sit amet consectetur adipisicing elit. Enim vel ipsum commodi laboriosam voluptate quidem fugiat, cum voluptatibus consectetur ex officiis, dolorum numquam perspiciatis porro labore? Soluta sint odio minima.",
       img: "https://images.pexels.com/photos/5077042/pexels-photo-5077042.jpeg?auto=compress&cs=tinysrgb&w=600",
     },
   ];
@@ -36,7 +36,12 @@ const Home = () => {
 
               </Link>
               <p>{post.description}</p>
+
+              <Link className="link" to={`/post/${post.id}`}>
+
               <button>Read More</button>
+              </Link>
+
             </div>
           </div>
         ))}
@@ -46,3 +51,4 @@ const Home = () => {
 };
 
 export default Home;
+
