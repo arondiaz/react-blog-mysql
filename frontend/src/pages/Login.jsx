85a19ff5bfc9d35e01c885ad543d57c20776e4d1
Arón Díaz <arondiaz.cpn@gmail.com>
2024-11-04 18:19:51 -0300

    updated login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,15 +1,66 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const Login = () => {
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setUser((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { username, password } = user;
+
+    if (username.trim() === "" || password.trim() === "") {
+      return setError("Invalid credentials");
+    }
+
+    try {
+      const url = `http://localhost:4040/api/auth/login`;
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+      });
+      const data = await response.json();
+      console.log(data);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      setError(error);
+    }
+  };
+
   return (
     <div className="auth">
       <h1>Login</h1>
       <form action="">
-        <input type="text" placeholder="username" />
-        <input type="text" placeholder="password" />
-        <button>Login</button>
-        <p>Login failed!</p>
-        <span>Don&apos;t have an account? <Link to={"/register"}>Register</Link></span>
+        <input
+          type="text"
+          placeholder="username"
+          name="username"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="password"
+          name="password"
+          onChange={handleChange}
+        />
+        <button onClick={handleSubmit}>Login</button>
+        {error && <p> {error} </p>}
+        <span>
+          Don&apos;t have an account? <Link to={"/register"}>Register</Link>
+        </span>
       </form>
     </div>
   );
