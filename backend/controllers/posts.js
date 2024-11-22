459851c3449f7b8de6d48cc1c19a0a9074aa529f
Arón Díaz <arondiaz.cpn@gmail.com>
2024-11-22 10:01:04 -0300

    created routes and controllers for post

--- backend/controllers/posts.js
+++ backend/controllers/posts.js
@@ -1,7 +1,19 @@
-export const showPost = (req, res) => {
-  res.json("Hola pepee");
-}
+export const getPosts = (req, res) => {
+  res.json("get posts");
+};
 
-export const showPepe = (req, res) => {
-  res.json("Hola pepeee");
-}
+export const getPost = (req, res) => {
+  res.json("get post");
+};
+
+export const addPost = (req, res) => {
+  res.json("add post");
+};
+
+export const deletePost = (req, res) => {
+  res.json("delete post");
+};
+
+export const updatePost = (req, res) => {
+  res.json("update post");
+};
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,9 +1,18 @@
 import express from "express";
-import {showPepe, showPost} from "../controllers/posts.js"
+import {
+  getPost,
+  getPosts,
+  addPost,
+  deletePost,
+  updatePost,
+} from "../controllers/posts.js";
 
 const router = express.Router();
 
-router.get("/test", showPost);
-router.get("/pepe", showPepe);
+router.get("/", getPost);
+router.get("/:id", getPosts);
+router.post("/", addPost);
+router.delete("/:id", deletePost);
+router.update("/:id", updatePost);
 
 export default router;
