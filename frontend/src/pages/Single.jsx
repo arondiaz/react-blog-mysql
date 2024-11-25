7f4fe62cd1db3ce69c13931afdbf942e3826f818
Arón Díaz <arondiaz.cpn@gmail.com>
2024-11-25 20:31:14 -0300

    implemented individual post page

--- frontend/src/pages/Single.jsx
+++ frontend/src/pages/Single.jsx
@@ -1,37 +1,58 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { Settings, Trash } from "lucide-react";
 import Menu from "../components/Menu";
+import { useState, useEffect, useContext } from "react";
+import moment from "moment";
+import AuthContext from "../context/authContext";
 
 const Single = () => {
+  const [post, setPost] = useState({});
+
+  const { currentUser } = useContext(AuthContext);
+
+  const location = useLocation();
+
+  let postId = location.pathname.split("/")[2];
+
+  useEffect(() => {
+    const url = `http://localhost:4040/api/post/${postId}`;
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setPost(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, [postId]);
+
+  const user = post[0];
   return (
     <div className="single">
       <div className="content">
-        <img
-          src="https://images.pexels.com/photos/16516/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=500"
-          alt=""
-        />
+        <img src={user?.img} alt="" />
         <div className="user">
-          <img
-            src="https://images.pexels.com/photos/28065063/pexels-photo-28065063/free-photo-of-creativo-azul-festivo-fiesta.jpeg?auto=compress&cs=tinysrgb&w=200"
-            alt=""
-          />
+          <img src={user?.userImg} alt="" />
           <div className="info">
-            <span>Pepe</span>
-            <p>posted 2 days ago</p>
+            <span>{user?.username}</span>
+            <p>posted {moment(user?.date).fromNow}</p>
           </div>
-          <div className="edit">
-            <Link to={`/write?edit=2`}>
-              <Settings />
-            </Link>
-            <Trash />
-          </div>
+          {currentUser?.username === user?.username && (
+            <div className="edit">
+              <Link to={`/write?edit=2`}>
+                <Settings />
+              </Link>
+              <Trash />
+            </div>
+          )}
         </div>
-        <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit, natus. Fugit error corporis maxime perspiciatis</h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum quas consequatur suscipit commodi molestias ipsam quod natus non quasi ratione. Quod in natus ducimus, fugiat placeat tempore porro deleniti illum!
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel aut dolores esse aspernatur voluptate fugiat sunt. Quisquam rem vitae est dolorum odio similique et ipsa, culpa delectus veniam maiores distinctio.
-        </p>
+        <h1>{user?.title}</h1>
+        <p>{user?.description}</p>
       </div>
-      <Menu/>
+      <Menu />
     </div>
   );
 };
